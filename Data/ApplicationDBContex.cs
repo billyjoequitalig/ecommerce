7507fa7f60bfda85e67b23e935e7d4979dc67709
Joe R. Quitalig <billyjoequitalig@gmail.com>
2025-09-27 19:14:19 +0800

    Add project files.

---
+++ Data/ApplicationDBContex.cs
@@ -0,0 +1,25 @@
+﻿using ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Electronics", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Clothing", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Books", DisplayOrder = 3 }
+            );
+            //ToTable("Categories");
+        }
+    }
+}
