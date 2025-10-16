73b5b91d948d655f2c6bbe0eb36494835c2f358b
Joe R. Quitalig <billyjoequitalig@gmail.com>
2025-10-17 02:39:54 +0800

    addcart

---
+++ Areas/Admin/Controllers/CompanyController .cs
@@ -0,0 +1,95 @@
+﻿using Jrq.DataAccess.Data;
+using Jrq.DataAccess.Repository.IRepository;
+using Jrq.Models;
+using Jrq.Models.Models.ViewModels;
+using Jrq.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+namespace Jrq.Ecommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = StaticDetails.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+        public IActionResult Index()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+
+            return View(objCompanyList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            
+            if (id == null || id == 0)
+            {
+                return View(new Company());
+            }
+            else
+            {
+               Company CompanyObj = _unitOfWork.Company.Get(u => u.Id == id);
+                return View(CompanyObj);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company CompanyObj)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                if (CompanyObj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(CompanyObj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(CompanyObj);
+                }
+                _unitOfWork.Save();
+                TempData["success"] = "Company Created Successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(CompanyObj);
+            }
+        }
+
+        
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = objCompanyList });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var CompanyToBeDelete = _unitOfWork.Company.Get(u => u.Id == id);
+            if (CompanyToBeDelete == null)
+            {
+
+                return Json(new { Success = false, Message = "Error While deleting" });
+            }
+            
+            _unitOfWork.Company.Remove(CompanyToBeDelete);
+            _unitOfWork.Save();
+            return Json(new { Success = false, Message = "Delete Successful" });
+
+        }
+        #endregion
+    }
+}
--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
+using System.Security.Claims;
 using Jrq.DataAccess.Repository.IRepository;
 using Jrq.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Jrq.Ecommerce.Areas.Customer.Controllers
@@ -25,8 +27,26 @@ namespace Jrq.Ecommerce.Areas.Customer.Controllers
 
         public IActionResult Details(int productId)
         {
-            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
-            return View(product);
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                Count = 1,
+                ProductId = productId
+            };
+            //Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
+            return View(cart);
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            shoppingCart.ApplicationUserId = userId;
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+            ;
         }
 
         public IActionResult Privacy()
